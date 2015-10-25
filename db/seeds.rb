7b011cf5e36078b1b603de53c4b0350a66af4035
robert <rboling91@gmail.com>
2015-10-25 05:44:57 -0400

    further updating seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,17 +12,17 @@
 #Merchant.create(name: 'Hamilton\'s sofa gallery', api_key: "r88gRhR/+nxTvSxdv9Te70blBlzSsSPb8ZYIjsGkjKs=", email: "na@na.com", bbb_accredited: true, bbb_rating: 13, feedzai_username: "robert-boling-2", feedzai_password: "vnrTayQMXu")
 
 # Merchant 1 payments
-PendingPayment.create(amount: 12000, currency: "USD", country: "US", time: DateTime.now, merchant_id: 1, is_deleted: false)
-PendingPayment.create(amount: 111000, currency: "USD", country: "US", time: DateTime.now, merchant_id: 1, is_deleted: false)
-PendingPayment.create(amount: 234000, currency: "USD", country: "US", time: DateTime.now, merchant_id: 1, is_deleted: false)
+#PendingPayment.create(amount: 12000, currency: "USD", country: "US", time: DateTime.now, merchant_id: 1, is_deleted: false)
+#PendingPayment.create(amount: 111000, currency: "USD", country: "US", time: DateTime.now, merchant_id: 1, is_deleted: false)
+#PendingPayment.create(amount: 234000, currency: "USD", country: "US", time: DateTime.now, merchant_id: 1, is_deleted: false)
 
 YelpReview.create(merchant_id: 1, stars: 1, review: "This place is a total nightmare!!!")
 YelpReview.create(merchant_id: 1, stars: 1, review: "Unfortunately, Mo and Damon have never been honest")
 
 # Merchant 2 payments
-PendingPayment.create(amount: 12000, currency: "USD", country: "US", time: DateTime.now, merchant_id: 2, is_deleted: false)
-PendingPayment.create(amount: 111000, currency: "USD", country: "US", time: DateTime.now, merchant_id: 2, is_deleted: false)
-PendingPayment.create(amount: 234000, currency: "USD", country: "US", time: DateTime.now, merchant_id: 2, is_deleted: false)
+#PendingPayment.create(amount: 12000, currency: "USD", country: "US", time: DateTime.now, merchant_id: 2, is_deleted: false)
+#PendingPayment.create(amount: 111000, currency: "USD", country: "US", time: DateTime.now, merchant_id: 2, is_deleted: false)
+#PendingPayment.create(amount: 234000, currency: "USD", country: "US", time: DateTime.now, merchant_id: 2, is_deleted: false)
 
 YelpReview.create(merchant_id: 2, stars: 5, review: "very friendly and helpful customer service")
 YelpReview.create(merchant_id: 2, stars: 5, review: "What a find. Quality. And I mean quality furniture")
